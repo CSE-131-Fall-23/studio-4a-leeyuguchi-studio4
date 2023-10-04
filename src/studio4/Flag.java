6cd953514801a0b2dc69c0125649443525e49853
Mia <Mia@localhost>
2023-10-04 15:43:51 -0500

    Finished Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,28 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		Color lightPurple = new Color(229, 204, 255);
+		Color medPurple = new Color(189, 132, 218);
+		Color darkPurple = new Color(154, 92, 185);
+		Color green = new Color(160, 189, 136);
+		
+		StdDraw.setPenColor(lightPurple);
+		StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.3);
+		
+		StdDraw.setPenColor(medPurple);
+		StdDraw.filledRectangle(0.5, 0.55, 0.4, 0.05);
+		StdDraw.filledRectangle(0.25, 0.5, 0.05, 0.3);
+		StdDraw.filledRectangle(0.45, 0.5, 0.05, 0.3);
+		
+		StdDraw.setPenColor(darkPurple);
+		StdDraw.filledSquare(0.25,0.55,0.05);
+		StdDraw.filledSquare(0.45, 0.55, 0.05);
+		
+		StdDraw.setPenColor(green);
+		StdDraw.filledEllipse(0.7, 0.5, 0.075, 0.25);
+		
+		StdDraw.setPenColor(darkPurple);
+		StdDraw.filledCircle(0.7, 0.5, 0.05);
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,48 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+
+		String shape = in.next();
+
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+
+		Color color = new Color(r, g, b);
+
+		boolean filled = in.nextBoolean();
+
+		double x1 = in.nextDouble();
+		double y1 = in.nextDouble();
+		double x2 = in.nextDouble();
+		double y2 = in.nextDouble();
+
+		if (shape .equals("rectangle")) {
+			StdDraw.setPenColor(color);
+			if (filled == true)
+			StdDraw.filledRectangle(x1, y1, x2, y2);
+			else
+			StdDraw.rectangle(x1, y1, x2, y2);
+		}
+		else if (shape .equals("ellipse")) {
+			StdDraw.setPenColor(color);
+			if (filled == true)
+			StdDraw.filledEllipse(x1, y1, x2, y2);
+			else
+			StdDraw.ellipse(x1, y1, x2, y2);
+		}
+		else if (shape .equals("triangle")) {
+			StdDraw.setPenColor(color);
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			double[] x = {x1, x2, x3};
+			double[] y = {y1, y2, y3};
+			if (filled == true)
+			StdDraw.filledPolygon(x, y);
+			else
+			StdDraw.polygon(x, y);
+		}
+
+
 	}
 }
